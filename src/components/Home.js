975a7d461da8082ca7bcd5290bf6b02da399e700
hristo2612 <hristoapps@gmail.com>
2018-09-02 01:05:08 +0300

    Connect Home.js component to redux state

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Layout from './Layout';
+import agent from '../agent';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
-const Home = () => {
+const Promise = global.Promise;
+
+const mapStateToProps = state => ({
+  appName: state.appName
+});
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: (payload) => dispatch({ type: 'HOME_PAGE_LOADED', payload }),
+});
+
+const Home = (props) => {
   return (
     <Layout>
-      <p>React Basic App</p>
+      <p>{props.appName}</p>
       <p>
-        <Link to="/dynamic">Dynamic Page</Link>
+        <Link to="/dynamic">
+          <Button variant="contained" color="primary">Dynamic Page</Button>
+        </Link>
       </p>
     </Layout>
   );
 };
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
