064e1a7751c26fe5b164e13a0fb57fc20221462e
hristo2612 <hristoapps@gmail.com>
2018-09-29 18:08:04 +0300

    Update App.css related styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import './styling/App.css';
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
